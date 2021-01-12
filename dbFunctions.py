f06feea394b3f2f666c398032f3f6081059e7782
mirela99 <adamache.mirela@yahoo.com>
2021-01-13 00:23:43 +0200

    update functions that interact with DB

--- dbFunctions.py
+++ dbFunctions.py
@@ -1,24 +1,62 @@
 from pymongo import MongoClient
+import pymongo
 
-
-# conexiunea cu baza de datee
+# database connection
 conn = MongoClient('mongodb://localhost:27017')
 db = conn.forbesBillionaires
-collection = db.people200
+collection = db.people1
 
 
-for i in collection.find({ "age": {'$ne': "" }} , {"name": 1, "age" : 2 }).sort('age').limit(10):
-    print(i)
+def youngest_10():
+    print("------------------------------------------")
+    print("Youngest 10 people in the first 200 forbes billionaires are: \n")
+    for i in collection.find({"age": {"$ne": ""}},
+                             {"forbes_id": 1,"name": 2, "age": 3, "_id": False}).sort([("age", 1), ("_id", pymongo.ASCENDING)]).limit(10):
+        print(i)
+    print("------------------------------------------")
+
 
 # collection.update({"age": ""}, { "$unset" : { "age" : 2 }})
 
 def us_citizenship():
+    print("------------------------------------------")
+    print("People with US citizenship are:\n ")
     count1 = 0
-    results = collection.find({"citizenship": "United States"})
+    results = collection.find({"citizenship": "United States"},
+                              {"forbes_id": 1, "name": 2, "age": 3, "citizenship": 4, "_id": False})
     for result in results:
         count1 = count1 + 1
-        print(result)
-    print("Persoane care au cetatenie americana ( United States ) :", count1)
-    count2 = 0
+    # print(result)
+    print("Number of people with US citizenship ( United States ) :", count1)
+    print("Number of people without US citizenship", (200 - count1))
+
+    print("------------------------------------------")
 
 
+def philantropyscore():
+    print("------------------------------------------")
+    print("Top 10 people with the highest philanthropy scores: \n")
+    for i in collection.find({"philanthropyScore": {'$ne': ""}},
+                             {"forbes_id": 1, "name": 2, "philanthropyScore": 3, "_id": False}).sort([("philanthropyScore", pymongo.DESCENDING),("_id", pymongo.ASCENDING)]).limit(10):
+        print(i)
+    print("------------------------------------------")
+    # for i in collection.find({ "$query":
+    #                          {"forbes_id": 1, "name": 2, "philanthropyScore": 3, "_id": False}},
+    #                           "$orderby":{"philanthropyScore:" -1}} ):
+    #     print(i)
+
+
+def rank():
+    print("------------------------------------------")
+    print(" People on same rank position in Forbes \n ")
+    position = input("Give us a number for the searched rank:")
+    for i in collection.find({"forbes_id": str(position)},
+                             {"forbes_id": 1, "name": 2, "age": 3, "_id": False}).sort("age", 1):
+        print(i)
+    print("------------------------------------------")
+
+
+youngest_10()
+us_citizenship()
+philantropyscore()
+rank()
